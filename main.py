fb0b4cfdd5dff8368ce144eaa64103177dc1d3b9
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2022-04-03 14:13:25 +0600

    some other ger req

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ def about_page():
     return {"data":{
         "Message": "This is About page!"
     }}
+
+
+@app.get("/blogs")
+def blog_list():
+    return {"data": ["This is blog list page!"]}
+
+
+@app.get("/blog/unpublished")
+def unpublished_blog_list():
+    return {"data": ["unpublished blogs"]}
+
+
+@app.get("/blog/{id}")
+def blog_list(id : int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def blog_list(id : int):
+    return {"data": f'comments of {id}'}

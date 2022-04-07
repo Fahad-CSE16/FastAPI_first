e47a608714f455d9a8a32b121d0e015f3fed6654
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2022-04-07 13:14:53 +0600

    list and retrieve done

--- blogapp/main.py
+++ blogapp/main.py
@@ -25,5 +25,12 @@ def create_blog(request:Blog, db:Session= Depends(get_db)):
     return new_blog
 
 
-# @app.get('/blogs')
-# def all_blogs():
+@app.get('/blogs')
+def all_blogs(db:Session= Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def single_blog(id, db:Session= Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    return blog

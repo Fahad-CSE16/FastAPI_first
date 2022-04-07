4bbfae001ffdcc5ac9ae9b2c32f162da0df8c60e
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2022-04-07 12:56:30 +0600

    db object creation added

--- blogapp/main.py
+++ blogapp/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from schemas import Blog
-from database import engine
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
 import models
 app = FastAPI()
 
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try :
+        yield db
+    finally:
+        db.close()
+
+
 @app.post("/create")
-def create_blog(request:Blog):
-    return request
+def create_blog(request:Blog, db:Session= Depends(get_db)):
+    new_blog = models.Blog(title=request.title, description = request.description)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+# @app.get('/blogs')
+# def all_blogs():

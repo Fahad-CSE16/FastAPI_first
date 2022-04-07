36b22c11ed55f6f5629f9d1d2db55ea914e25bcb
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2022-04-07 12:43:25 +0600

    db connections and first model added

--- blogapp/main.py
+++ blogapp/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from schemas import Blog
+from database import engine
+import models
 app = FastAPI()
 
+
+models.Base.metadata.create_all(engine)
 
 @app.post("/create")
 def create_blog(request:Blog):

1dd7e4d9e6318500db591b3455426ebad6789689
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2022-04-18 14:47:48 +0600

    authorization used

--- blogapp/routers/blog.py
+++ blogapp/routers/blog.py
@@ -14,8 +14,8 @@ router = APIRouter(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_blog(request:Blog, db:Session= Depends(get_db)):
-    new_blog = models.Blog(title=request.title, description = request.description, created_by_id=1)
+def create_blog(request:Blog, db:Session= Depends(get_db), current_user: User = Depends(get_current_user)):
+    new_blog = models.Blog(title=request.title, description = request.description, created_by_id=current_user.id)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -30,7 +30,7 @@ def all_blogs(response:Response, db:Session= Depends(get_db), current_user: User
     return blogs
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=ShowBlog)
-def single_blog(id, response:Response, db:Session= Depends(get_db)):
+def single_blog(id, response:Response, db:Session= Depends(get_db), current_user: User = Depends(get_current_user)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
         raise HTTPException(detail=f'Not found for id {id}', status_code=status.HTTP_404_NOT_FOUND)
@@ -39,7 +39,7 @@ def single_blog(id, response:Response, db:Session= Depends(get_db)):
     return blog
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_blog(id, request:Blog, db:Session= Depends(get_db)):
+def update_blog(id, request:Blog, db:Session= Depends(get_db), current_user: User = Depends(get_current_user)):
     blog = db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
         raise HTTPException(detail=f'Not found for id {id}', status_code=status.HTTP_404_NOT_FOUND)
@@ -48,7 +48,7 @@ def update_blog(id, request:Blog, db:Session= Depends(get_db)):
     return {'details':"updated"}
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_single_blog(id, response:Response, db:Session= Depends(get_db)):
+def delete_single_blog(id, response:Response, db:Session= Depends(get_db), current_user: User = Depends(get_current_user)):
     blog = db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
         raise HTTPException(detail=f'Not found for id {id}', status_code=status.HTTP_404_NOT_FOUND)
